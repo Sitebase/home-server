3e7296503c9f9df17b23d068dbbafaaf6e503831
Wim Mostmans <wim@sitebase.be>
2014-09-21 10:54:14 +0200

    fix typo

--- puppet/modules/plex/manifests/init.pp
+++ puppet/modules/plex/manifests/init.pp
@@ -9,7 +9,7 @@ class plex {
 	}
 
 	exec { "download":                                                                                                                     
-        command => "wget -O plex.dev http://downloads.plexapp.com/plex-media-server/0.9.9.14.531-7eef8c6/plexmediaserver_0.9.9.14.531-7eef8c6_amd64.deb",                                                         
+        command => "wget -O plex.deb http://downloads.plexapp.com/plex-media-server/0.9.9.14.531-7eef8c6/plexmediaserver_0.9.9.14.531-7eef8c6_amd64.deb",                                                         
         cwd => "/tmp",
         creates => "/tmp/plex.deb",                                                              
         require => User['plex'],
